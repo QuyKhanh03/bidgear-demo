6ba023fcd4077095cffbf210bbc52bdfedd8af53
khanhpn <khanh.pn@bidgear.com>
2024-07-23 17:30:10 +0700

    add router

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div>
     <nav>
+      <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/test-1">test-1</NuxtLink>
       <NuxtLink to="/test-2">test-2</NuxtLink>
     </nav>
--- pages/test-1.vue
+++ pages/test-1.vue
@@ -8,6 +8,7 @@
     </aside>
 
     <nav>
+      <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/test-1">test-1</NuxtLink>
       <NuxtLink to="/test-2">test-2</NuxtLink>
     </nav>
