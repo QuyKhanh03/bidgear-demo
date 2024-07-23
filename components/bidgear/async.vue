fa3ffae4558c0a89aabb6a2fdc06c50f2ba260fc
khanhpn <khanh.pn@bidgear.com>
2024-07-23 17:41:11 +0700

    fix adcash

--- components/bidgear/async.vue
+++ components/bidgear/async.vue
@@ -1,28 +1,36 @@
 <template>
-    <div>
-      <div id="bg-ssp-9332"></div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    mounted() {
-      const script = document.createElement('script');
-      script.async = true;
-      script.setAttribute('data-cfasync', 'false');
-      script.src = 'https://platform.bidgear.com/pubbidgear-ad.js';
-      document.head.appendChild(script);
-  
-      const bg_id = document.getElementById('bg-ssp-9332');
-      bg_id.id = 'bg-ssp-9332-' + Math.floor(Math.random() * Date.now());
-  
-      window.pubbidgeartag = window.pubbidgeartag || [];
-      window.pubbidgeartag.push({
-        zoneid: 9332,
-        id: bg_id.id,
-        wu: window.location.href
-      });
-    }
-  };
-  </script>
-  
+  <div>
+    <div id="bg-ssp-9332"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Create and append the pubbidgear script
+    const scriptPubbidgear = document.createElement('script');
+    scriptPubbidgear.async = true;
+    scriptPubbidgear.setAttribute('data-cfasync', 'false');
+    scriptPubbidgear.src = 'https://platform.bidgear.com/pubbidgear-ad.js';
+    document.head.appendChild(scriptPubbidgear);
+
+    // Create and append the aclib script
+    const scriptAclib = document.createElement('script');
+    scriptAclib.id = 'aclib';
+    scriptAclib.type = 'text/javascript';
+    scriptAclib.src = '//acscdn.com/script/aclib.js';
+    document.head.appendChild(scriptAclib);
+
+    // Update the bg-ssp-9332 ID and set up pubbidgeartag
+    const bg_id = document.getElementById('bg-ssp-9332');
+    bg_id.id = 'bg-ssp-9332-' + Math.floor(Math.random() * Date.now());
+
+    window.pubbidgeartag = window.pubbidgeartag || [];
+    window.pubbidgeartag.push({
+      zoneid: 9332,
+      id: bg_id.id,
+      wu: window.location.href
+    });
+  }
+}
+</script>
