cb71df414995c258817b72884035afb6def6baf7
khanhpn <khanh.pn@bidgear.com>
2024-07-26 10:05:14 +0700

    add clear cookie

--- components/bidgear/async.vue
+++ components/bidgear/async.vue
@@ -1,3 +1,42 @@
+<!-- <template>
+  <div>
+    <div id="bg-ssp-9332"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Create and append the pubbidgear script
+    const scriptPubbidgear = document.createElement('script');
+    scriptPubbidgear.async = true;
+    scriptPubbidgear.setAttribute('data-cfasync', 'false');
+    scriptPubbidgear.src = 'https://platform.bidgear.com/pubbidgear-ad.js';
+    document.head.appendChild(scriptPubbidgear);
+
+    // Create and append the aclib script
+    const scriptAclib = document.createElement('script');
+    scriptAclib.id = 'aclib';
+    scriptAclib.type = 'text/javascript';
+    scriptAclib.src = '//acscdn.com/script/aclib.js';
+    document.head.appendChild(scriptAclib);
+
+    // Update the bg-ssp-9332 ID and set up pubbidgeartag
+    const bg_id = document.getElementById('bg-ssp-9332');
+    bg_id.id = 'bg-ssp-9332-' + Math.floor(Math.random() * Date.now());
+
+    window.pubbidgeartag = window.pubbidgeartag || [];
+    window.pubbidgeartag.push({
+      zoneid: 9332,
+      id: bg_id.id,
+      wu: window.location.href
+    });
+  }
+}
+</script> -->
+
+
+
 <template>
   <div>
     <div id="bg-ssp-9332"></div>
@@ -6,35 +45,44 @@
 
 <script>
 export default {
-  head() {
-    return {
-      script: [
-        {
-          hid: 'pubbidgear',
-          src: 'https://platform.bidgear.com/pubbidgear-ad.js',
-          async: true,
-          'data-cfasync': 'false',
-          type: 'text/javascript'
-        },
-        
-      ]
-    };
-  },
   mounted() {
+    this.clearAllCookies();
+
+    // Create and append the pubbidgear script
+    const scriptPubbidgear = document.createElement('script');
+    scriptPubbidgear.async = true;
+    scriptPubbidgear.setAttribute('data-cfasync', 'false');
+    scriptPubbidgear.src = 'https://platform.bidgear.com/pubbidgear-ad.js';
+    document.head.appendChild(scriptPubbidgear);
+
+    // Create and append the aclib script
+    const scriptAclib = document.createElement('script');
+    scriptAclib.id = 'aclib';
+    scriptAclib.type = 'text/javascript';
+    scriptAclib.src = '//acscdn.com/script/aclib.js';
+    document.head.appendChild(scriptAclib);
+
     // Update the bg-ssp-9332 ID and set up pubbidgeartag
     const bg_id = document.getElementById('bg-ssp-9332');
-    if (bg_id) {
-      const uniqueId = 'bg-ssp-9332-' + Math.floor(Math.random() * Date.now());
-      bg_id.id = uniqueId;
+    bg_id.id = 'bg-ssp-9332-' + Math.floor(Math.random() * Date.now());
 
-      window.pubbidgeartag = window.pubbidgeartag || [];
-      window.pubbidgeartag.push({
-        zoneid: 9332,
-        id: uniqueId,
-        wu: window.location.href
-      });
-    } else {
-      console.error('Element with id bg-ssp-9332 not found');
+    window.pubbidgeartag = window.pubbidgeartag || [];
+    window.pubbidgeartag.push({
+      zoneid: 9332,
+      id: bg_id.id,
+      wu: window.location.href
+    });
+  },
+  methods: {
+    clearAllCookies() {
+      const cookies = document.cookie.split(";");
+
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+      }
     }
   }
 }
